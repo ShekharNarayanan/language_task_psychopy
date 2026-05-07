9b7069347fe0baacde8f9f99f3db30e64b96c572
Shekhar Narayanan <s.narayanan@tilburguniversity.edu>
2026-05-07 12:11:37 +0200

    Add sample experiment

--- main.py
+++ main.py
@@ -1,6 +1,31 @@
-def main():
-    print("Hello from language-task-psychopy!")
+from psychopy import visual, event, core, monitors
 
 
 if __name__ == "__main__":
-    main()
+    mon = monitors.Monitor(name="my_monitor")
+    mon.setSizePix((1920, 1080))
+    mon.setWidth(52)
+    mon.setDistance(60)
+
+    win = visual.Window(
+        size=(1920, 1080),
+        monitor=mon,
+        color="gray",
+        fullscr=False,
+        units="deg",
+        screen=0,
+        waitBlanking=True,  # syncs to 60hz refresh
+    )
+    banner = visual.TextStim(win,text="Press space to exit",pos=(0, 8),height=2,color='black')
+    fixation = visual.TextStim(win, text="+", color="black", height=1)
+
+    while True:
+        banner.draw()
+        fixation.draw()
+        win.flip()
+
+        if "space" in event.getKeys():
+            break
+
+    win.close()
+    core.quit()
