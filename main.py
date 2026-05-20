9ec056480b1dc32fb89c95503903b8d329fa42eb
Shekhar Narayanan <s.narayanan@tilburguniversity.edu>
2026-05-20 12:23:16 +0200

    add monitor specs and remove initial psychopy warning

--- main.py
+++ main.py
@@ -2,20 +2,29 @@ from psychopy import visual, event, core, monitors
 
 
 if __name__ == "__main__":
-    mon = monitors.Monitor(name="my_monitor")
-    mon.setSizePix((1920, 1080))
-    mon.setWidth(52)
-    mon.setDistance(60)
+    # from psychopy import visual, monitors
+
+    # Define monitor specifications
+    monitorname = 'my_lab_monitor'
+    width_cm = 53.1       # Physical width of the screen
+    distance_cm = 60.0    # Distance from participant to screen
+    width_px = 1920       # Horizontal pixels
+    height_px = 1080      # Vertical pixels
+
+    # Create Monitor Object
+    mon = monitors.Monitor(monitorname, width=width_cm, distance=distance_cm)
+    mon.setSizePix((width_px, height_px))
 
     win = visual.Window(
-        size=(1920, 1080),
-        monitor=mon,
-        color="gray",
-        fullscr=False,
-        units="deg",
-        screen=0,
-        waitBlanking=True,  # syncs to 60hz refresh
-    )
+    size=(1080, 720), 
+    checkTiming=False, 
+    infoMsg="", 
+    monitor=mon,
+    units='deg',  # Units now scaled properly using your monitor specs
+    fullscr=True
+
+)
+
     banner = visual.TextStim(win,text="Press space to exit",pos=(0, 8),height=2,color='black')
     fixation = visual.TextStim(win, text="+", color="black", height=1)
 
