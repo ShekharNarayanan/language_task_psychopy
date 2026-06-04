66203407975de98624c5bbe0b615909ea2fcd41f
Shekhar Narayanan <s.narayanan@tilburguniversity.edu>
2026-06-04 12:14:38 +0200

    add module for test instructions

---
+++ screens/instruction.py
@@ -0,0 +1,30 @@
+"""
+instruction.py
+--------------
+Generic instruction screen that displays a block of text and advances on SPACE.
+"""
+
+from psychopy import visual, event
+
+
+def run_instruction(win, text, text_color):
+    """
+    Display an instruction screen and wait for SPACE to continue.
+
+    Args:
+        win:        PsychoPy Window object.
+        text:       Instruction string to display.
+        text_color: Color of the text (string or RGB).
+    """
+    stim = visual.TextStim(
+        win, text=text,
+        pos=(0, 0), height=1.0,
+        color=text_color,
+        wrapWidth=30   # wraps long text within the screen
+    )
+
+    while True:
+        stim.draw()
+        win.flip()
+        if 'space' in event.getKeys():
+            break
